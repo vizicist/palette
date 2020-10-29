abb7de799cf1f0f9feb55722711958ae2d0316db
Tim Thompson <me@timthompson.com>
2020-10-29 12:56:28 -0700

    script cleanup

---
+++ local_palette6/bin/palettestopall.bat
@@ -0,0 +1,4 @@
+@echo off
+call stoppalette
+call stopbidule
+call stopresolume
--- scripts/startremote.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call "%PALETTE%\bin\killall.bat"
-call "%PALETTE%\bin\startpalette.bat"
-rem give NATS, etc time to finish starting
-sleep 5
-call "%PALETTE%\bin\startgui.bat"
