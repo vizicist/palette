82ed1f6751deed43f7f6e658583aecd69f5f5531
Tim Thompson <me@timthompson.com>
2020-10-30 20:14:00 -0700

    tweak palettestart

--- scripts/palettestart.bat
+++ scripts/palettestart.bat
@@ -1,6 +1,15 @@
+@echo off
+rem Provide an argument of "full" to 
+if "%1" == "" goto remote
+set guitype=%1
+goto keepgoing
+:remote
+set guitype=remote
+:keepgoing
 
 c:/windows/system32/taskkill /F /IM palette.exe > nul 2>&1
 c:/windows/system32/taskkill /F /IM palette_gui_remote.exe > nul 2>&1
+c:/windows/system32/taskkill /F /IM palette_gui_full.exe > nul 2>&1
 
 set logdir=%LOCALAPPDATA%\Palette\logs
 
@@ -15,4 +24,4 @@ sleep 4
 echo > "%logdir%\gui.log"
 echo > "%logdir%\gui.stdout"
 echo > "%logdir%\gui.stderr"
-start /b "" "%PALETTE%\bin\pyinstalled\palette_gui_remote.exe" > "%logdir%\gui.stdout" 2> "%logdir%\gui.stderr"
+start /b "" "%PALETTE%\bin\pyinstalled\palette_gui_%guitype%.exe" > "%logdir%\gui.stdout" 2> "%logdir%\gui.stderr"
