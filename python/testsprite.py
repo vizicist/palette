eb4cf51df00164dd59866038deafb8db24824025
Tim Thompson <me@timthompson.com>
2020-11-08 17:50:06 -0800

    Create testsprite.py

---
+++ python/testsprite.py
@@ -0,0 +1,23 @@
+import sys
+import time
+import random
+import palette
+
+if len(sys.argv) > 1:
+        ntimes = int(sys.argv[1])
+else:
+        ntimes = 10
+
+if len(sys.argv) > 2:
+        dt = float(sys.argv[2])
+else:
+        dt = 0.1
+
+cid = "0"
+palette.FakeNUID(palette.MyNUID()+"_test")
+for n in range(ntimes):
+    x = random.random()
+    y = random.random()
+    z = random.random() / 4.0
+    palette.SendSpriteEvent(cid,x,y,z)
+    time.sleep(dt)
