009723a15e9676fefa925a806f283d536bcff476
Tim Thompson <vizicist@gmail.com>
2020-11-10 17:56:25 -0800

    Update palette_midifile.py

--- python/palette_midifile.py
+++ python/palette_midifile.py
@@ -20,14 +20,26 @@ def doNote2Cursor(args,timesofar,msg):
 def doNote2Sprite(args,timesofar,msg):
     if msg.type != "note_on":
         return
+
+    # do piano-roll-like positioning - time in X direction, pitch in Y
     x = timesofar * args.xfactor
+    midpitch = 64
+    dpitch = (msg.note - midpitch)
+    y = 0.5 + dpitch * args.yfactor
+    # print("note=",msg.note," dpitch=",dpitch," y=",y)
+
     x = x % 1.0
-    y = msg.note / 128.0
+    y = y % 1.0
+
     cid = "one"
     palette.SendSpriteEvent(cid,x,y,0.5)
 
 def doNote2MIDI(args,timesofar,msg):
     palette.SendMIDIEvent(args.device,timesofar,msg)
+
+def doNote2SpriteMIDI(args,timesofar,msg):
+    doNote2MIDI(args,timesofar,msg)
+    doNote2Sprite(args,timesofar,msg)
 
 def doNote2Debug(args,timesofar,msg):
     print("%f : %s" % (timesofar,str(msg)) )
@@ -51,7 +63,8 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--id", help="TBD")
     parser.add_argument("-t", "--timefactor", type=float, help="Playback time factor", default=1.0)
-    parser.add_argument("-x", "--xfactor", type=float, help="X delta for placements", default=1.0)
+    parser.add_argument("-x", "--xfactor", type=float, help="X factor for sprite placement", default=0.1)
+    parser.add_argument("-y", "--yfactor", type=float, help="Y factor for sprite placement", default=0.01)
     parser.add_argument("-v", "--verbosity", action="count", default=0)
     parser.add_argument("-d", "--device", default="palette_midifile")
     parser.add_argument("midifile")
@@ -68,6 +81,8 @@ if __name__=="__main__":
         palette.SendMIDITimeReset()
     elif args.generate == "sprite":
         dofunc = doNote2Sprite
+    elif args.generate == "spritemidi":
+        dofunc = doNote2SpriteMIDI
     elif args.generate == "debug":
         dofunc = doNote2Debug
     else:
