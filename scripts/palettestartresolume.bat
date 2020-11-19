9f90bc7c465d4568fa4d8a86ce6bfe7f63cbc913
Tim Thompson <me@timthompson.com>
2020-11-19 12:54:40 -0800

    windows doesn't have sleep cmd

--- scripts/palettestartresolume.bat
+++ scripts/palettestartresolume.bat
@@ -4,7 +4,7 @@ call palettestopresolume.bat
 start /b "" "C:\\Program Files\\Resolume Avenue 6\\Avenue.exe"
 
 rem give it time to start before sending it OSC
-sleep 4
+timeout /t 4 > nul
 
 set osc="%PALETTE%\bin\pyinstalled\osc.exe"
 %osc% send 7000@127.0.0.1 /composition/layers/1/clips/1/connect 1
@@ -13,7 +13,7 @@ set osc="%PALETTE%\bin\pyinstalled\osc.exe"
 %osc% send 7000@127.0.0.1 /composition/layers/4/clips/1/connect 1
 
 rem another try in case Resolume takes longer to start
-sleep 4
+timeout /t 4 > nul
 
 set osc="%PALETTE%\bin\pyinstalled\osc.exe"
 %osc% send 7000@127.0.0.1 /composition/layers/1/clips/1/connect 1
