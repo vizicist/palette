42044a1da7b1388f3bb91a2945a0fbce7b42b61b
Tim Thompson <me@timthompson.com>
2020-11-20 14:50:08 -0800

    add ffgl7

---
+++ ffgl7/README.md
@@ -0,0 +1,49 @@
+This is the Resolume fork of the FFGL repository. It is up to date and has Visual Studio and Xcode projects to compile 64 bit plugins that can be loaded by Resolume 7.0.3 and up.
+
+The master branch is used for continued development. It will contain the latest features, fixes and bugs. Plugins compiled with the master branch will work in Resolume 7.3.1 and up.
+If you do not want to be affected by the latest bugs you can use one of the stable releases. eg FFGL 2.2, which is the most recent released version of the sdk. Plugin development for Resolume 7.0.0/7.0.1/7.0.2 is no longer supported by this repository. These versions are very old and there are many newer versions that users can update to.
+
+You can find some help to get started with FFGL plugin development on the [wiki](https://github.com/resolume/ffgl/wiki).
+
+Also more examples are available on this [repo](https://github.com/flyingrub/ffgl/tree/more/).
+
+## Master branch changes since FFGL 2.2
+*None yet, you can suggest a change by creating an issue. In the issue describe the problem that has to be solved and if you want a suggestion on how it could be solved.*
+
+
+## Quickstart
+
+Below are the first steps you need to create and test an FFGL plugin for Resolume. This assumes you have experience with git and C++ development.
+
+### Mac
+
+- Go to `<repo>/build/osx`, open `FFGLPlugins.xcodeproj`
+- Create a compilation target for your plugin:
+	- Select the Xcode project (top of the tree)
+	- Duplicate a target and rename it
+	- Remove the old plugin-specific files under Build Phases > Compile Sources (e.g. if you duplicated Gradients, remove `FFGLGradients.cpp`)
+	- Duplicating a target in Xcode creates and assigns a new `xx copy-Info.plist` file, but we don't want that. Go to Build Settings > Packaging > Info.plist and change the file name to `FFGLPlugin-Info.plist`.  
+	- Find the reference to the newly created `xx copy-Info.plist` file in the Xcode Project Navigator (probably all the way down the panel) and remove it there. When asked, choose Move to Trash.
+- In Finder, duplicate a plugin folder and rename the files. Choose a corresponding plugin type, e.g. copy `AddSubtract` if you want to build an Effect plugin or `Gradients` if you want to build a Source plugin.
+- Drag the new folder into the Xcode project. You will be asked to which target you want to add them, add them to your new target.
+- Go to the target's Build Phases again and make sure there are no resources under the Copy Bundle Resources phase.
+- Replace the class names to match your new plugin name and rename the elements in the PluginInfo struct
+- Fix up the Build scheme:
+	- When duplicating a target, a Build Scheme was also created. Next to the play and stop buttons, click the schemes dropdown and select Manage Schemes. 
+	- Rename the scheme that was auto-created (e.g. "Gradient copy")
+	- Select it in the scheme drop down.
+- Press play (Cmd+B) to compile.
+- Copy the resulting `.bundle` file from `<repo>/binaries/debug` to `~/Documents/Resolume/Extra Effects` and start Arena to test it.
+
+### Windows 
+
+This assumes you use Visual Studio 2017
+
+- Go to `<repo>/build/windows`, duplicate a `.vcxproj` and the corresponding `.vcxproj.filters` file, and rename them.
+- Open `FFGLPlugins.sln`. Then right-click the Solution in the solution explorer (top of the tree), and choose Add > Existing Project and select the new file.
+- Remove the original `.cpp` and `.h` source files from the newly added project, i.e. if you duplicated `Gradient.vcxproj`, remove `FFGLGradients.h` and `FFGLGradients.cpp`
+- In Explorer, go to `<repo>/source/`, duplicate a plugin folder and rename the files. Choose a corresponding plugin type, i.e. copy `AddSubtract` if you want to build an Effect plugin or `Gradients` if you want to build a Source plugin.
+- Add the new source files to the project by dragging them into Visual Studio, onto your new project.
+- If you want to start the build with Visual Studio's Build command (F5), right-click the project and select Set as Startup Project. Altenatively, you can right-click the project and select Build.
+- After building, find the resulting `.dll` file in `\binaries\x64\Debug`. Copy it to `<user folder>/Documents/Resolume/Extra Effects`
+
---
+++ ffgl7/source/lib/palette/TrackedCursor.cpp
@@ -0,0 +1,161 @@
+#include "PaletteAll.h"
+
+bool TrackedCursor::initialized = false;
+bool TrackedCursor::Debug = false;
+
+// std::vector<std::string> TrackedCursor::behaviourTypes;
+
+TrackedCursor::TrackedCursor(Palette* palette_, std::string cid, std::string cidsource, Region* region_, NosuchVector pos_, double z) {
+	// _area = area_;
+	_palette = palette_;
+	// _last_pitches.clear();
+	// _last_channel = -1;
+	// _last_click = -1;
+	_cidsource = cidsource;
+	_cid = cid;
+	_region = region_;
+
+	if ( Debug) {
+		NosuchDebug("NEW TrackedCursor region=%d cid=%s source=%s",_region, cid.c_str(),_cidsource.c_str());
+	}
+
+	curr_raw_pos = pos_;
+	// NosuchDebug("Setting curr_raw_pos and _last_raw_pos to %f %f",curr_raw_pos.x,curr_raw_pos.y);
+
+	_target_pos = pos_;
+	_last_raw_pos = pos_;
+
+	// _prev_pos = pos_;
+	_last_raw_depth = z;
+	curr_raw_depth = z;
+	_target_depth = z;
+	NosuchDebug(2,"Cursor CONSTRUCTOR cid=%s/%s  pos_ = %.3f %.3f",_cid.c_str(),_cidsource.c_str(),pos_.x,pos_.y);
+	_start_time = Palette::now;
+	_last_tm = _start_time;
+	_last_instantiate = 0;
+
+	curr_degrees = 0.0f;
+	_smooth_degrees_factor = 0.2f;
+	// _target_degrees = 0.0f;
+
+	_g_firstdir = true;
+	_touched = 0;
+}
+
+void TrackedCursor::initialize() {
+
+	if ( initialized )
+		return;
+	initialized = true;
+
+	// behaviourTypes.push_back("instantiate"); // instantiate sprites continuously
+	// behaviourTypes.push_back("move");        // instantiate and then move a single sprite
+	// behaviourTypes.push_back("accumulate");  // continuously accumulate points for a single sprite
+}
+
+TrackedCursor::~TrackedCursor() {
+	if ( Debug ) {
+		NosuchDebug("TrackedCursor DESTRUCTOR!!  this=%llx _cid=%s/%s",(long long)this,_cid.c_str(),_cidsource.c_str());
+	}
+}
+
+double
+normalize_degrees(double d) {
+	if ( d < 0.0f ) {
+		d += 360.0f;
+	} else if ( d > 360.0f ) {
+		d -= 360.0f;
+	}
+	return d;
+}
+
+void
+TrackedCursor::advanceTo(int tm) {
+
+	int dt = tm - _last_tm;
+	if ( dt <= 0 ) {
+		return;
+	}
+
+	if ( curr_raw_pos.x == _target_pos.x && curr_raw_pos.y == _target_pos.y ) {
+		NosuchDebug(1,"Cursor::advanceTo, current and target are the same");
+		return;
+	}
+
+	NosuchVector dpos = _target_pos.sub(curr_raw_pos);
+	double raw_distance = dpos.mag();
+
+	// Not sure why I have this here, maybe just for sanity check
+	if ( raw_distance > 1.5f ) {
+		NosuchDebug("Cursor::advanceTo, raw_distance>1.5 !?");
+		return;
+	}
+	if ( raw_distance == 0.0f ) {
+		NosuchDebug("Cursor::advanceTo, raw_distance=0.0 !?");
+		return;
+	}
+
+	_last_raw_pos = curr_raw_pos;
+
+	int smoothxyz_factor = 1 + region()->params.smoothxyz;
+
+	dpos = dpos.normalize();
+
+	// curr_raw_pos = curr_raw_pos.sub(dpos);
+	// curr_raw_pos = _target_pos;
+
+	double old_x = curr_raw_pos.x;
+	curr_raw_pos.x = ((smoothxyz_factor-1)*curr_raw_pos.x + _target_pos.x) / (double)smoothxyz_factor;
+	curr_raw_pos.y = ((smoothxyz_factor-1)*curr_raw_pos.y + _target_pos.y) / (double)smoothxyz_factor;
+
+	// XXX - should be taking time (dt) into account
+	curr_raw_depth = ((smoothxyz_factor-1)*curr_raw_depth + target_depth()) / (double)smoothxyz_factor;
+
+	/////////////// smooth the degrees
+	double tooshort = 0.01f; // 0.05f;
+	if (raw_distance < tooshort) {
+		// NosuchDebug("   raw_distance=%.3f too small %s\n",
+		// 	raw_distance,DebugString().c_str());
+	} else {
+		NosuchVector dp = curr_raw_pos.sub(_last_raw_pos);
+		double heading = dp.heading();
+		// NosuchDebug("");
+		// NosuchDebug("HEADING %f",heading);
+		_target_degrees = radian2degree(heading);
+		_target_degrees += 90.0;
+		_target_degrees = normalize_degrees(_target_degrees);
+
+		if (_g_firstdir) {
+			curr_degrees = _target_degrees;
+			_g_firstdir = false;
+		} else {
+			double dd1 = _target_degrees - curr_degrees;
+			double dd;
+			if ( dd1 > 0.0f ) {
+				if ( dd1 > 180.0f ) {
+					dd = -(360.0f - dd1);
+				}
+				else {
+					dd = dd1;
+				}
+			} else {
+				if ( dd1 < -180.0f ) {
+					dd = dd1 + 360.0f;
+				}
+				else {
+					dd = dd1;
+				}
+			}
+			// double smooth_degrees_factor = _smooth_degrees_factor;
+			double smooth_degrees_factor = 1.0f / (smoothxyz_factor * 10);
+
+			curr_degrees = curr_degrees + (dd*smooth_degrees_factor);
+			curr_degrees = normalize_degrees(curr_degrees);
+		}
+		// NosuchDebug("   FINAL current_degrees =%.4f",curr_degrees);
+	}
+
+	_last_tm = Palette::now;
+
+	// NosuchDebug("   end of advanceTo %s",DebugString().c_str());
+}
