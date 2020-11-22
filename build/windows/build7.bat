b856c07933b55db515c26bb4dad4456ceef4fd0a
Tim Thompson <vizicist@gmail.com>
2020-11-22 13:11:44 -0800

    rework things to use ABCD rather than 1234

--- build/windows/build7.bat
+++ build/windows/build7.bat
@@ -1,14 +1,16 @@
+set ship=%PALETTESOURCE%\build\windows\ship
+
 echo ================ Compiling FFGL7 plugin
 set MSBUILDCMD=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\vsmsbuildcmd.bat
-call "%MSBUILDCMD%"
+call "%MSBUILDCMD%" > nul
 pushd %PALETTESOURCE%\ffgl7\build\windows
-msbuild /t:Build /p:Configuration=Debug /p:Platform="x64" palette.sln
+msbuild /t:Build /p:Configuration=Debug /p:Platform="x64" FFGLPlugins.sln > nul
 popd
+echo on
 
 echo ================ Copying FFGL7 plugin
-pushd %PALETTESOURCE%\ffgl7\windows\x64\Debug
 mkdir %ship%\ffgl7
+pushd %PALETTESOURCE%\ffgl7\binaries\x64\Debug
 copy Palette*.dll %ship%\ffgl7 > nul
 copy %PALETTESOURCE%\build\windows\pthreadvc2.dll %ship%\ffgl7 >nul
 popd
-
