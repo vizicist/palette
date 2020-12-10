12fa3a182bdb4ed98ef28526d0ea72f2802fa262
Tim Thompson <vizicist@gmail.com>
2020-12-09 21:18:11 -0800

    ffgl7->ffgl, lots of work on presets

--- python/changeparamval.py
+++ python/changeparamval.py
@@ -3,32 +3,40 @@ import glob
 import sys
 import os
 
-def changeparam(paramfile,paramname,newval):
+def changeparam(paramfile,paramname,oldval,newval):
     f = open(paramfile)
     params = json.load(f)
     f.close()
 
-    if not paramname in params["params"]:
-        print "Hey, param ",paramname," isn't in the params!?"
-        sys.exit(1)
+    for ch in {"A","B","C","D"}:
+        chname = ch + "_" + paramname
+        if chname in params["params"]:
+            if params["params"][chname] == oldval:
+                print("Changing chname=",chname)
+                params["params"][chname] = str(newval)
 
-    params["params"][paramname]["value"] = str(newval)
+    if paramname in params["params"]:
+        if params["params"][paramname] == oldval:
+            print("Changing parmname=",paramname)
+            params["params"][paramname] = str(newval)
 
-    print "Writing ",paramfile
+    print("Writing ",paramfile)
     f = open(paramfile,"w")
     f.write(json.dumps(params, sort_keys=True, indent=4, separators=(',',':'))) 
+    f.write("\n")
     f.close()
 
-if len(sys.argv) < 3:
-    print "usage: %s [paramdir] [paramname] [newval]" % sys.argv[0]
+if len(sys.argv) < 4:
+    print("usage: %s [paramdir] [paramname] [oldval] [newval]" % sys.argv[0])
     sys.exit(1)
 
 paramdir = sys.argv[1]
 paramname = sys.argv[2]
-newval = sys.argv[3]
+oldval = sys.argv[3]
+newval = sys.argv[4]
 
 files = glob.glob(os.path.join(paramdir,'*.json'))
 for s in files:
-    print "file = ",s
-    changeparam(s,paramname,newval)
+    print("file = ",s)
+    changeparam(s,paramname,oldval,newval)
 
---
+++ scripts/gogetpackages.bat
@@ -0,0 +1,5 @@
+go get github.com/hypebeast/go-osc/osc
+go get github.com/nats-io/nats-server/server
+go get github.com/nats-io/nats.go
+go get github.com/nats-io/nuid
+go get gopkg.in/mail.v2
