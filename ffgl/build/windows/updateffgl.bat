bd7f67d5261291b40ffcb470a9fac0c469dfe2ba
Tim Thompson <vizicist@gmail.com>
2020-12-10 14:26:44 -0800

    Lots of preset work

--- ffgl/build/windows/updateffgl.bat
+++ ffgl/build/windows/updateffgl.bat
@@ -1,5 +1,5 @@
 call palettestopresolume
-timeout /t 3 > nul
+delay 3
 copy ..\..\binaries\x64\Debug\Palette.dll "c:\Program Files\Palette\ffgl"
 copy ..\..\binaries\x64\Debug\Palette.pdb "c:\Program Files\Palette\ffgl"
 copy ..\..\..\default\config\paramdefs.json "c:\Program Files\Palette\config\paramdefs.json"
