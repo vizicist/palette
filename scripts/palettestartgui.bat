652207f4f8d38906be4b86f2694fb54caa58c047
Tim Thompson <vizicist@gmail.com>
2021-07-11 15:23:42 -0700

    First alpha of 5.01

--- scripts/palettestartgui.bat
+++ scripts/palettestartgui.bat
@@ -3,9 +3,15 @@
 c:/windows/system32/taskkill /F /IM palette_gui.exe > nul 2>&1
 
 call setpalettelogdir
+
 if not "%PALETTELOGDIR%" == "" (
 	echo > "%PALETTELOGDIR%\gui.log"
 	echo > "%PALETTELOGDIR%\gui.stdout"
 	echo > "%PALETTELOGDIR%\gui.stderr"
+)
+
+if not "%PALETTESOURCE%" == "" (
+	start /b "" "%PALETTESOURCE%\build\windows\ship\bin\pyinstalled\palette_gui.exe" > "%PALETTELOGDIR%\gui.stdout" 2> "%PALETTELOGDIR%\gui.stderr"
+) else (
 	start /b "" "%PALETTE%\bin\pyinstalled\palette_gui.exe" > "%PALETTELOGDIR%\gui.stdout" 2> "%PALETTELOGDIR%\gui.stderr"
 )
