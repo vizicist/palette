33318c2c69453789e6dc4e497bad19f00ea8d0c4
Tim Thompson <vizicist@gmail.com>
2021-08-09 17:22:26 -0700

    include morphs.json

--- build/windows/buildsetup.bat
+++
@@ -1,4 +0,0 @@
-echo ================ Creating installer
-"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q palette_win_setup.iss
-move Output\palette_*_win_setup.exe %PALETTESOURCE%\release >nul
-rmdir Output
