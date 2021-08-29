3fa976da40318913d37085c07d798527f3b25196
Tim Thompson <vizicist@gmail.com>
2021-08-29 00:28:30 -0700

    clean up .bat files

--- scripts/palettestart.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-call palettestartengine
-call palettestartgui
--- scripts/palettestartbidule.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call palettestopbidule.bat
-set patch="%LOCALAPPDATA%\Palette\config\palette.bidule"
-echo Starting Bidule on %patch%
-start /b "" "C:\Program Files\Plogue\Bidule\PlogueBidule_x64.exe" %patch%
