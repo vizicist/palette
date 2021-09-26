97aa7696ceff4ecff4282f142fe8da05ee16c8d1
Tim Thompson <vizicist@gmail.com>
2021-09-25 17:47:03 -0700

    Final tweaks, might be done with 5.7 release

--- build/windows/buildinstall.bat
+++ build/windows/buildinstall.bat
@@ -1,5 +1,5 @@
-call build.bat
 call palette stop all
+call build.bat
 set /p version=<../../VERSION
 set INSTALLER=..\..\release\palette_%version%_win_setup.exe
 copy %INSTALLER% t:\tjt\media\spacepalettepro\installers >nul
