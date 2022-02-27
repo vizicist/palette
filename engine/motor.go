bdeaa3ab192fd79e0acc9d510bdbe56ac2110671
Tim Thompson <vizicist@gmail.com>
2022-02-26 20:30:35 -0800

    cli for get/set of param values is working

--- engine/motor.go
+++ engine/motor.go
@@ -1,9 +1,7 @@
 package engine
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"runtime"
@@ -340,227 +338,6 @@ func (motor *Motor) AdvanceByOneClick() {
 	}
 }
 
-// ExecuteAPI xxx
-func (motor *Motor) ExecuteAPI(api string, args map[string]string, rawargs string) (result string, err error) {
-
-	if Debug.MotorAPI {
-		log.Printf("MotorAPI: api=%s rawargs=%s\n", api, rawargs)
-	}
-
-	// ALL visual.* APIs get forwarded to the FreeFrame plugin inside Resolume
-	if strings.HasPrefix(api, "visual.") {
-		msg := osc.NewMessage("/api")
-		msg.Append(strings.TrimPrefix(api, "visual."))
-		msg.Append(rawargs)
-		motor.toFreeFramePluginForLayer(msg)
-	}
-
-	switch api {
-
-	case "set":
-		// if preset, category gets prepended to all the parameter names.
-		prefix := ""
-		category, okcategory := args["category"]
-		if okcategory {
-			prefix = category + "."
-		}
-		for name, value := range args {
-			if name != "category" {
-				motor.setOneParamValue(prefix+name, value)
-			}
-		}
-
-	case "get":
-		result = ""
-		sep := ""
-		prefix := ""
-		// if preset, category gets prepended to all the parameter names.
-		category, okcategory := args["category"]
-		if okcategory {
-			prefix = category + "."
-		}
-		for nm := range motor.params.values {
-			name := prefix + nm
-			val := motor.params.paramValueAsString(name)
-			result += fmt.Sprintf("%s{\"name\":\"%s\",\"value\":\"%s\"}", sep, name, val)
-			sep = ","
-		}
-
-	case "load":
-		preset, okpreset := args["preset"]
-		if !okpreset {
-			return "", fmt.Errorf("missing preset parameter")
-		}
-		log.Printf("Should be loading preset=%s\n", preset)
-		path := ReadablePresetFilePath(preset)
-		bytes, err := ioutil.ReadFile(path)
-		if err != nil {
-			return "", err
-		}
-		var f interface{}
-		err = json.Unmarshal(bytes, &f)
-		if err != nil {
-			return "", fmt.Errorf("unable to Unmarshal path=%s, err=%s", path, err)
-		}
-		toplevel := f.(map[string]interface{})
-		params, okparams := toplevel["params"]
-		if !okparams {
-			return "", fmt.Errorf("no params value in jsom")
-		}
-		paramsmap, okmap := params.(map[string]interface{})
-		if !okmap {
-			return "", fmt.Errorf("params value is not a map in jsom")
-		}
-		// If the preset value is of the form {category}.{preset},
-		// then we pull off the category and add it as a prefix
-		// to the parameter names.
-		prefix := ""
-		i := strings.Index(preset, ".")
-		if i >= 0 {
-			prefix = preset[0 : i+1]
-		}
-		for nm := range paramsmap {
-			i := paramsmap[nm]
-			s, oks := i.(string)
-			if !oks {
-				log.Printf("nm=%s value isn't a string in params json", nm)
-			}
-			log.Printf("paramsmap nm=%s s=%s\n", nm, s)
-			fullname := prefix + nm
-			motor.setOneParamValue(fullname, s)
-		}
-		return "", nil
-
-	case "save":
-		preset, okpreset := args["preset"]
-		if !okpreset {
-			return "", fmt.Errorf("missing preset parameter")
-		}
-		log.Printf("Should be saving preset=%s\n", preset)
-		wantCategory, _ := PresetNameSplit(preset)
-		path := WriteablePresetFilePath(preset)
-		s := ""
-		sep := "{\n    \"params\": {\n"
-		for nm := range motor.params.values {
-			thisCategory, thisPreset := PresetNameSplit(nm)
-			if thisCategory != wantCategory {
-				continue
-			}
-			s += fmt.Sprintf("%s        \"%s\":\"%s\"", sep, thisPreset, motor.params.paramValueAsString(nm))
-			sep = ",\n"
-		}
-		s += "\n    }\n}"
-		data := []byte(s)
-		err := ioutil.WriteFile(path, data, 0644)
-		return "", err
-
-	case "loop_recording":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil {
-			motor.loopIsRecording = v
-		} else {
-			err = e
-		}
-
-	case "loop_playing":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil && v != motor.loopIsPlaying {
-			motor.loopIsPlaying = v
-			motor.terminateActiveNotes()
-		} else {
-			err = e
-		}
-
-	case "loop_clear":
-		motor.loop.Clear()
-		motor.clearGraphics()
-		motor.sendANO()
-
-	case "loop_comb":
-		motor.loopComb()
-
-	case "loop_length":
-		i, e := needIntArg("length", api, args)
-		if e == nil {
-			nclicks := Clicks(i)
-			if nclicks != motor.loop.length {
-				motor.loop.SetLength(nclicks)
-			}
-		} else {
-			err = e
-		}
-
-	case "loop_fade":
-		f, e := needFloatArg("fade", api, args)
-		if e == nil {
-			motor.fadeLoop = f
-		} else {
-			err = e
-		}
-
-	case "ANO":
-		motor.sendANO()
-
-	case "midi_thru":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil {
-			motor.MIDIThru = v
-		} else {
-			err = e
-		}
-
-	case "midi_setscale":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil {
-			motor.MIDISetScale = v
-		} else {
-			err = e
-		}
-
-	case "midi_usescale":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil {
-			motor.MIDIUseScale = v
-		} else {
-			err = e
-		}
-
-	case "clearexternalscale":
-		motor.clearExternalScale()
-		motor.MIDINumDown = 0
-
-	case "midi_quantized":
-		v, err := needBoolArg("onoff", api, args)
-		if err == nil {
-			motor.MIDIQuantized = v
-		}
-
-	case "midi_thruscadjust":
-		v, e := needBoolArg("onoff", api, args)
-		if e == nil {
-			motor.MIDIThruScadjust = v
-		} else {
-			err = e
-		}
-
-	case "set_transpose":
-		v, e := needIntArg("value", api, args)
-		if e == nil {
-			motor.TransposePitch = v
-			if Debug.Transpose {
-				log.Printf("motor API set_transpose TransposePitch=%v", v)
-			}
-		} else {
-			err = e
-		}
-
-	default:
-		err = fmt.Errorf("Motor.ExecuteAPI: unknown api=%s", api)
-	}
-
-	return result, err
-}
-
 // HandleMIDITimeReset xxx
 func (motor *Motor) HandleMIDITimeReset() {
 	log.Printf("HandleMIDITimeReset!! needs implementation\n")
@@ -604,12 +381,14 @@ func CallerFunc() string {
 	return funcname
 }
 
-func (motor *Motor) setOneParamValue(fullname, value string) {
+func (motor *Motor) SetOneParamValue(fullname, value string) error {
 
-	motor.params.SetParamValueWithString(fullname, value, nil)
+	err := motor.params.SetParamValueWithString(fullname, value, nil)
+	if err != nil {
+		return err
+	}
 
 	if strings.HasPrefix(fullname, "visual.") {
-		log.Printf("Should setOneParamValue be setting visual.* params to FF?")
 		name := strings.TrimPrefix(fullname, "visual.")
 		msg := osc.NewMessage("/api")
 		msg.Append("set_params")
@@ -623,6 +402,8 @@ func (motor *Motor) setOneParamValue(fullname, value string) {
 		// Effect parameters get sent to Resolume
 		motor.sendEffectParam(name, value)
 	}
+
+	return nil
 }
 
 // ClearExternalScale xxx
