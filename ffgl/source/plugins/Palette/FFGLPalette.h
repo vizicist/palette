6258196e2867293f66818cd851dc78467bc3ae20
Tim Thompson <vizicist@gmail.com>
2022-06-05 15:48:57 -0700

    Add PaletteEffect ffgl plugin, initially empty

--- ffgl/source/plugins/Palette/FFGLPalette.h
+++ ffgl/source/plugins/Palette/FFGLPalette.h
@@ -5,10 +5,10 @@
 class FFGLPalette : public CFFGLPlugin
 {
 public:
-	FFGLPalette(std::string configfile);
+	FFGLPalette();
 	~FFGLPalette() { }
 
-	// override methods in CFFGLPlugin
+	//CFFGLPlugin
 	FFResult InitGL(const FFGLViewportStruct* vp) override;
 	FFResult ProcessOpenGL(ProcessOpenGLStruct* pGL) override;
 	FFResult DeInitGL() override;
@@ -17,37 +17,7 @@ public:
 	FFResult SetTextParameter(unsigned int index, const char* value) override;
 	char* GetTextParameter(unsigned int index) override;
 
+protected:
 	PaletteHost* paletteHost;
-
-	///////////////////////////////////////////////////
-	// Factory method
-	///////////////////////////////////////////////////
-
-	static FFResult __stdcall CreateInstance(CFFGLPlugin** ppOutInstance)
-	{
-		// The search for ffgl.json is as follows:
-		// - look in %CommonProgramFiles%
-		// - last resort is temp dir
-
-		std::string jsonpath;
-
-		char* localValue;
-		size_t locallen;
-		errno_t localerr = _dupenv_s(&localValue, &locallen, "CommonProgramFiles");
-		if (!localerr && localValue != NULL) {
-			jsonpath = std::string( localValue ) + "\\Palette\\config\\ffgl.json";
-			free( localValue );
-		} else {
-			jsonpath = "c:\\windows\\temp\\ffgl.json";// last resort
-			NosuchDebug("No value for CommonProgramFiles? using jsonpath=%s\n", jsonpath.c_str());
-		}
-
-		*ppOutInstance = new FFGLPalette( jsonpath );
-		if( *ppOutInstance != NULL )
-			return FF_SUCCESS;
-		return FF_FAIL;
-	}
-
-private:
 
 };
