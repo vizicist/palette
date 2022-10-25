9c734d1d646c7c7c33b287cd0775ef12e7751129
Tim Thompson <vizicist@gmail.com>
2022-10-25 15:42:26 -0700

    checkpoint UNWORKING code
    Update router.go

---
+++ responder/responder_demo.go
@@ -0,0 +1,47 @@
+package responder
+
+import (
+	"log"
+	"time"
+
+	"github.com/vizicist/palette/engine"
+)
+
+type Responder_demo struct {
+}
+
+func NewResponder_demo() Responder_demo {
+	p := Responder_demo{}
+	return p
+}
+
+/////////////////////////// external interface
+
+func (r Responder_demo) OnCursorDeviceEvent(ce engine.CursorDeviceEvent) {
+	log.Printf("NewResponder_demo in OnCursorEvent!\n")
+	if ce.Ddu == "down" {
+		go func() {
+			note := cursorToNote(ce)
+			log.Printf("cursor down: publishing note=%s\n", note.String())
+			engine.SendNoteToSynth(note)
+			time.Sleep(2 * time.Second)
+			note.TypeOf = "noteoff"
+			engine.SendNoteToSynth(note)
+		}()
+	}
+	log.Printf("OnCursorEvent in responder_demo called! ce=%v\n", ce)
+}
+
+/////////////////////////// internal things
+
+func cursorToNote(ce engine.CursorDeviceEvent) *engine.Note {
+	pitch := int(ce.X * 126.0)
+	_ = pitch
+	s := "+b"
+	note, err := engine.NoteFromString((s))
+	if err != nil {
+		log.Printf("xyzToNote: bad note - %s\n", s)
+		note, _ = engine.NoteFromString("+a")
+	}
+	return note
+}
