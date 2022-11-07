3c60e183e6da2079d6aa01ea0b746848e7de3435
Tim Thompson <vizicist@gmail.com>
2022-11-07 13:39:39 -0800

    more Responder hacking

--- engine/responder.go
+++ engine/responder.go
@@ -7,29 +7,62 @@ import (
 )
 
 type ResponderManager struct {
-	responders       map[string]Responder
-	activeResponders map[string]Responder
-	mutex            sync.RWMutex
+	responders        map[string]Responder
+	respondersContext map[string]*ResponderContext
+	activeResponders  map[string]Responder
+	mutex             sync.RWMutex
+}
+
+type ResponderContext struct {
+	scheduler *Scheduler
 }
 
 type Responder interface {
-	OnCursorEvent(e CursorEvent, rm *ResponderManager)
+	OnCursorEvent(ctx *ResponderContext, e CursorEvent)
 }
 
 func NewResponderManager() *ResponderManager {
 	return &ResponderManager{
-		responders:       make(map[string]Responder),
-		activeResponders: make(map[string]Responder),
-		mutex:            sync.RWMutex{},
+		responders:        make(map[string]Responder),
+		respondersContext: make(map[string]*ResponderContext),
+		activeResponders:  make(map[string]Responder),
+		mutex:             sync.RWMutex{},
 	}
 }
 
-func (rm *ResponderManager) AddResponder(name string, resp Responder) {
+func NewResponderContext() *ResponderContext {
+	return &ResponderContext{
+		scheduler: TheEngine().Scheduler,
+	}
+}
+
+func (ctx *ResponderContext) CurrentClick() Clicks {
+	return CurrentClick()
+}
+
+func (ctx *ResponderContext) ScheduleNoteNow(nt *Note) {
+	click := CurrentClick()
+	log.Printf("ResponderContext.ScheduleNow: nt=%s clk=%d\n", nt, click)
+	ctx.scheduler.ScheduleNoteAt(nt, click)
+}
+
+func (ctx *ResponderContext) ScheduleNoteAt(nt *Note, click Clicks) {
+	if nt == nil {
+		log.Printf("ResponderContext.ScheduleNoteAt: nt == nil?\n")
+		return
+	}
+	log.Printf("ResponderContext.ScheduleAt: nt=%s clicks=%d\n", nt, click)
+}
+
+func (rm *ResponderManager) AddResponder(name string, responder Responder) {
 	_, ok := rm.responders[name]
 	if !ok {
-		rm.responders[name] = resp
+		log.Printf("ResponderManager.AddResponder: name=%s\n", name)
+		rc := NewResponderContext()
+		rm.responders[name] = responder
+		rm.respondersContext[name] = rc
 	} else {
-		log.Printf("Warning: ResponderManager.AddResponder is overwriting old name=%s\n", name)
+		log.Printf("Warning: ResponderManager.AddResponder can't overwriting existing responder=%s\n", name)
 	}
 }
 
@@ -55,6 +88,11 @@ func (rm *ResponderManager) DeactivateResponder(name string) error {
 func (rm *ResponderManager) handleCursorEvent(ce CursorEvent) {
 	for name, responder := range rm.responders {
 		log.Printf("CallResponders: name=%s\n", name)
-		responder.OnCursorEvent(ce, rm)
+		context, ok := rm.respondersContext[name]
+		if !ok {
+			log.Printf("ResponderManager.handle: no context for name=%s\n", name)
+		} else {
+			responder.OnCursorEvent(context, ce)
+		}
 	}
 }
