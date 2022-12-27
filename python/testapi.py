f978a525fdfe39b638db2c895b5f37c9f4123e61
Tim Thompson <vizicist@gmail.com>
2022-12-27 09:49:08 -0800

    fix python, delayed stop

--- python/testapi.py
+++ python/testapi.py
@@ -1,4 +1,4 @@
 import palette
 
-r = palette.palette_api("\"api\":\"engine.echo\",\"value\":\"THIS IS THE ECHO\"")
-print("r=",r)
+r, err = palette.palette_api("\"api\":\"engine.echo\",\"value\":\"THIS IS THE ECHO\"")
+print("r=",r," err=",err)
