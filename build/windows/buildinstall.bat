cf7fd378a806be6fd054d155c6b22c030617e582
spacepalette <spacepalette@gmail.com>
2023-05-06 16:24:22 -0700

    make buildinstall smoother

--- build/windows/buildinstall.bat
+++ build/windows/buildinstall.bat
@@ -1,6 +1,3 @@
 rem call palette stop
 call build.bat
-set /p version=<../../VERSION
-set INSTALLER=..\..\release\palette_%version%_win_setup.exe
-rem copy %INSTALLER% t:\tjt\media\spacepalettepro\installers >nul
-%INSTALLER%
+call installonly.bat
