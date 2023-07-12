728a3021a54cf64c95fe69c2e7b831ad5b6d98f3
Tim Thompson <vizicist@gmail.com>
2023-07-12 16:41:53 -0700

    change where logs are stored

--- scripts/clearlogs.bat
+++ scripts/clearlogs.bat
@@ -1,6 +1,6 @@
 @echo off
 call setpalettelogdir
-del /s %PALETTELOGDIR%\*.log >nul 2>nul
-del /s %PALETTELOGDIR%\*.stderr >nul 2>nul
-del /s %PALETTELOGDIR%\*.stdout >nul 2>nul
-echo Logs in %PALETTELOGDIR% have been cleared
+del /s %PALETTE_LOGDIR%\*.log >nul 2>nul
+del /s %PALETTE_LOGDIR%\*.stderr >nul 2>nul
+del /s %PALETTE_LOGDIR%\*.stdout >nul 2>nul
+echo Logs in %PALETTE_LOGDIR% have been cleared
