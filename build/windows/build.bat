7f11828f2141b665d4ac6521ebd27b49e9969661
Tim Thompson <me@timthompson.com>
2023-10-18 18:36:18 -0700

    add dlls

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -170,6 +170,8 @@ copy %PALETTE_SOURCE%\SenselLib\x64\LibSenselDecompress.dll %bin% >nul
 rem copy %PALETTE_SOURCE%\depthlib\build\x64\Release\depthlib.dll %bin% >nul
 rem copy vc15\bin\depthai-core.dll %bin% >nul
 rem copy vc15\bin\opencv_world454.dll %bin% >nul
+copy "%USERPROFILE%\mingw64\bin\libgcc_s_sjlj-1.dll" %bin% >nul
+copy "%USERPROFILE%\mingw64\bin\libstdc++-6.dll" %bin% >nul
 
 echo ================ Removing unused things
 rm -fr %bin%\pyinstalled\tcl\tzdata
