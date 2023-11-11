e10b780734fbd42c2e1d232382e803948ec15c66
Tim Thompson <vizicist@gmail.com>
2023-11-11 12:16:50 -0800

    delete old stuff

--- scripts/cdnotes.bat
+++
@@ -1,2 +0,0 @@
-c:
-cd %USERPROFILE%\Documents\Github\notes-from-nosuchtim
--- scripts/setkeyvars.bat
+++
@@ -1,6 +0,0 @@
-set KEYOUT=Microsoft GS Wavetable Synth
-rem set KEYPATH=%USERPROFILE%\Documents\GitHub\palette\data_omnisphere\keykit\liblocal;%USERPROFILE%\Documents\GitHub\palette\keykit\lib
-set KEYPATH=%USERPROFILE%\Documents\GitHub\palette\data_omnisphere\keykit\liblocal;C:\Program Files\palette\keykit\lib
-set KEYPAGEPERSISTENT=%USERPROFILE%\Documents\GitHub\palette\data_omnisphere\keykit\liblocal\palette_persistent.kp
-set KEYSIZE=["x0"=100,"y0"=100,"x1"=900,"y1"=600]
-set KEYALLOW=127.0.0
