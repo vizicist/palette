19d2d5d408401cd8f55d781c41c2906eabb960e7
Tim Thompson <vizicist@gmail.com>
2024-05-29 17:55:55 -0700

    build with kinect

--- build/windows/buildinstaller.bat
+++ build/windows/buildinstaller.bat
@@ -7,6 +7,15 @@ echo ================ Creating installer for VERSION %version%
 
 sed -e "s/SUBSTITUTE_VERSION_HERE/%version%/" < palette_win_setup.iss > tmp.iss
 "c:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q tmp.iss
+
+if not "%PALETTE_MMTT%" == "kinect" goto no_kinect
+move Output\palette_%version%_win_setup.exe %PALETTE_SOURCE%\release\palette_%version%_win_setup_with_kinect.exe >nul
+goto finish
+
+:no_kinect
 move Output\palette_%version%_win_setup.exe %PALETTE_SOURCE%\release >nul
+
+:finish
+
 rmdir Output
 rm tmp.iss
