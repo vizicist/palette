2b62e9d00cd026c9ddb6173cb2ebc3af88534c87
Tim Thompson <vizicist@gmail.com>
2024-06-06 14:31:09 -0700

    final tweaks for 7.98

--- scripts/palette_onboot.bat
+++ scripts/palette_onboot.bat
@@ -1,12 +1,4 @@
-rem start /b palette_splash -image=sppro_rebooting.png -width=1920 -height=700
 echo on
-
-call cddata.bat
-echo on
-cd
-if exist config\tv_on.bat call config\tv_on.bat
 
 rem bash checkmorphs.sh
-rem palette start
-rem timeout /t 600 > nul
-rem taskkill /f /im palette_splash.exe
+palette start
