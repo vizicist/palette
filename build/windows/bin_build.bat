e6a322e6b27d621d34cb788e6d21d767ac595047
Tim Thompson <vizicist@gmail.com>
2024-06-23 16:22:48 -0700

    first 8.*

--- build/windows/bin_build.bat
+++ build/windows/bin_build.bat
@@ -89,7 +89,7 @@ popd
 if not "%PALETTE_MMTT%" == "kinect" goto no_mmtt_kinect
 	echo ================ Compiling mmtt_kinect
 	pushd %PALETTE_SOURCE%\mmtt_kinect\build\windows
-	msbuild /t:Build /p:Configuration=Debug /p:Platform="x32" mmtt_kinect.sln > nul
+	msbuild /t:Build /p:Configuration=Debug /p:Platform="x32" mmtt_kinect.sln
 	rem Put mmtt_kinect in its own bin directory, to keep 32-bit things separate
 	mkdir %bin%\mmtt_kinect
 	copy mmtt_kinect\Debug\mmtt_kinect.exe %bin%\mmtt_kinect\mmtt_kinect.exe >nul
