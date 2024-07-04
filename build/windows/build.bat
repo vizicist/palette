de001cde1457850565cd742a8b27245bcbd50ca7
Tim Thompson <vizicist@gmail.com>
2024-07-04 15:00:54 -0700

    cleanup and build data_* stuff

--- build/windows/build.bat
+++ build/windows/build.bat
@@ -1,9 +1,6 @@
 echo off
+set PALETTE_MMTT=none
 echo "BUILDING binaries..."
 call bin_build
 echo "BUILDING data..."
 call data_build
-echo "INSTALLING binaries"
-call bin_install
-echo "INSTALLING data"
-call data_install
---
+++ scripts/setkinect.bat
@@ -0,0 +1,1 @@
+set PALETTE_MMTT=kinect
