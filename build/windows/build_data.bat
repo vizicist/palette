a6639fb0d1658c88fc48dbf7fbb0f64631eecd78
spacepalette <spacepalette@gmail.com>
2024-09-02 17:53:36 -0700

    rework data installers to add sfmoma

--- build/windows/build_data.bat
+++ build/windows/build_data.bat
@@ -3,8 +3,12 @@
 set /p version=<../../VERSION
 set PALETTE_VERSION=%version%
 
-set data=%PALETTE_DATA%
-if "%data%" == "" set data=omnisphere
+set data=%1
+
+if not "%data%" == "" goto keepgoing0
+	echo You must provide an argument, e.g. "omnisphere" or "sfmoma"
+	goto getout
+:keepgoing0
 
 if not "%PALETTE_SOURCE%" == "" goto keepgoing1
 	echo You must set the PALETTE_SOURCE environment variable.
@@ -12,18 +16,19 @@ if not "%PALETTE_SOURCE%" == "" goto keepgoing1
 :keepgoing1
 
 set ship=%PALETTE_SOURCE%\build\windows\ship
-set datadir=data_%data%
 
 rm -fr %ship% > nul 2>&1
 mkdir %ship%
 
+set datadir=data_%data%
 echo ================ Copying %datadir%
 mkdir %ship%\%datadir%
 xcopy /e /y %PALETTE_SOURCE%\%datadir%\* %ship%\%datadir% >nul
 
 echo ================ Creating installer for %datadir%
 
-"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q data_omnisphere.iss
+set PALETTE_DATA=%data%
+"c:\Program Files (x86)\Inno Setup 6\ISCC.exe" /Q data.iss
 
 move Output\%datadir%_%version%.exe %PALETTE_SOURCE%\release\palette_%version%_%datadir%.exe >nul
 
