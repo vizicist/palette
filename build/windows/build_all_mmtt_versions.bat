0ab9fcfd78e59a0fa680ad616294d2cc36a2edea
Tim Thompson <vizicist@gmail.com>
2024-09-03 20:46:23 -0700

    complete build with kinect version

--- build/windows/build_all_mmtt_versions.bat
+++ build/windows/build_all_mmtt_versions.bat
@@ -2,9 +2,9 @@
 palette stop
 echo "====================== creating build with PALETTE_MMTT set to none"
 set PALETTE_MMTT=none
-call cleanall.bat
+call clean.bat
 call build.bat
 echo "====================== creating build with PALETTE_MMTT set to kinect"
 set PALETTE_MMTT=kinect
-call cleanall.bat
+call clean.bat
 call build.bat
