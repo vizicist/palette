a06d536c53b86978f0f790a9ab2df1e9476a3652
Tim Thompson <vizicist@gmail.com>
2024-11-13 17:32:27 -0800

    update to visual studio 2022

--- scripts/msdev22.bat
+++ scripts/msdev22.bat
@@ -1,2 +1,3 @@
 rem set VCTargetsPath=C:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V140\
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+set VSCMD_DEBUG=1
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" > vcvars64.out 2> vcvars64.err
